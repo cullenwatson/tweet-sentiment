900164de51d2274d747cb166eafb4c75d00871c1
cgwatson193 <78247585+cgwatson193@users.noreply.github.com>
2021-09-07 18:46:22 -0500

    changed build.yaml

--- main.cpp
+++ main.cpp
@@ -7,28 +7,28 @@ int main(int argc, char* argv[])
     // check to make sure user entered file's needed for program execution
     if (argc ==1) {
         test();
+    }else{
+        // get tweets
+        readTrainFile(argv[1]);
+
+        // get words
+        addWords(positiveTweets, positiveWords);
+        addWords(negativeTweets, negativeWords);
+
+        // add test tweets and predicted sentiment to vector of type Tweets
+        readTestFile(argv[2]);
+
+        // read in actual sentiment and id's and put in another Tweet vector
+        getActualSentiment(argv[3]);
+
+        // compare the results of the two Tweet vectors
+        printResults(argv[4]);
+
+
+        cout << "done" << endl;
     }
 
-    // get tweets
-    readTrainFile(argv[1]);
-
-    // get words
-    addWords(positiveTweets, positiveWords);
-    addWords(negativeTweets, negativeWords);
-
-    // add test tweets and predicted sentiment to vector of type Tweets
-    readTestFile(argv[2]);
-
-    // read in actual sentiment and id's and put in another Tweet vector
-    getActualSentiment(argv[3]);
-
-    // compare the results of the two Tweet vectors
-    printResults(argv[4]);
-
-
-    cout << "done" << endl;
     return 0;
-
 }
 int test(){
     return Catch::Session().run();
